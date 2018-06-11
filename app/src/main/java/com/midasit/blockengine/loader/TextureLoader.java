7b9f9a1c2df04d9595f334737f343b7875a3abc5
tura <soldier4443@gmail.com>
2018-06-11 17:55:40 +0900

    TextureLoader 추가

---
+++ app/src/main/java/com/midasit/blockengine/loader/TextureLoader.java
@@ -0,0 +1,53 @@
+package com.midasit.blockengine.loader;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.opengl.GLES20;
+import android.opengl.GLUtils;
+
+import static android.opengl.GLES20.GL_CLAMP_TO_EDGE;
+import static android.opengl.GLES20.GL_TEXTURE_2D;
+import static android.opengl.GLES20.GL_TEXTURE_WRAP_S;
+import static android.opengl.GLES20.GL_TEXTURE_WRAP_T;
+import static android.opengl.GLES20.glBindTexture;
+import static android.opengl.GLES20.glTexParameteri;
+import static com.midasit.blockengine.lwjgl.GLES.glGenTextures;
+
+/**
+ * Created by nyh0111 on 2018-06-11.
+ */
+public class TextureLoader {
+    public static int loadTexture(Context context, int resourceId) {
+        try {
+            final BitmapFactory.Options options = new BitmapFactory.Options();
+            options.inScaled = false;   // No pre-scaling
+        
+            // Read in the resource
+            final Bitmap bitmap = BitmapFactory.decodeResource(context.getResources(), resourceId, options);
+        
+            int id = glGenTextures();
+            glBindTexture(GL_TEXTURE_2D, id);
+        
+            // Set filtering
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+        
+            glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+            glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+        
+            // Load the bitmap into the bound texture.
+            GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
+        
+            // Recycle the bitmap, since its data has been loaded into OpenGL.
+            bitmap.recycle();
+        
+            return id;
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+    
+        return -1;
+    }
+}
